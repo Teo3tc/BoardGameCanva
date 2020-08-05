f95cbd7d926b5493ce5e79fc9a2dad7b58d451a8
teo <theo.etchanchu@gmail.com>
2020-08-05 13:22:30 +0200

    05/08/20

--- Canvas/src/js/move.js
+++ Canvas/src/js/move.js
@@ -1,6 +1,7 @@
 class Move {
-    static makeDataMoveleft(player, celuleObstacle, move) {
 
+
+    static makeDataMoveleft(player, celuleObstacle, move) {
         let moveLeft = [];
         let width = player.width - 50;
         let height = player.height;
@@ -9,25 +10,19 @@ class Move {
         move == 2 ? moveLeftAfterObstacle = 100 : moveLeftAfterObstacle
         move == 1 ? moveLeftAfterObstacle = 50 : moveLeftAfterObstacle
         move == 0 ? moveLeftAfterObstacle = 0 : moveLeftAfterObstacle
-        console.log('------SHOW ME ----');
-
         for (let index = 0; index < move; index++) {
             moveLeft.push({
                 width,
                 height
             })
             width = width - 50
-            celuleObstacle.forEach((obstacle) => {
-                if (
-                    obstacle.width == moveLeft[index].width &&
-                    obstacle.height == moveLeft[index].height
-                ) {
-                    saveObstacle.push(obstacle.width);
+            celuleObstacle.forEach(obstacle => {
+                if (obstacle.width == moveLeft[index].width && obstacle.height == moveLeft[index].height) {
+                    saveObstacle.push(obstacle.width)
                 }
             });
 
         }
-
         for (let y = 0; y < saveObstacle.length; y++) {
             if (moveLeft[0].width == saveObstacle[0]) {
 
@@ -36,212 +31,180 @@ class Move {
                 if (moveLeft[1].width == saveObstacle[1] || moveLeft[1].width == saveObstacle[0]) {
 
                     moveLeftAfterObstacle = 50
-                    move = 50
+
 
                 } else if (moveLeft[1].width != saveObstacle[y]) {
                     if (moveLeft[2].width == saveObstacle[y]) {
 
                         moveLeftAfterObstacle = 100
-                        move = 100
+
                     } else if (
                         moveLeft[2].width != saveObstacle[y] &&
                         moveLeft[1].width != saveObstacle[y]
                     ) {
 
                         moveLeftAfterObstacle = 150
-                        move = 150
+
 
                     }
                 }
             }
         }
         player.move.left = moveLeftAfterObstacle;
-    }
+    };
     static makeDataMoveTop(player, celuleObstacle, move) {
-
+        let moveLeft = [];
+        let width = player.width;
+        let height = player.height - 50;
         let saveObstacle = [];
-        let moveLeftAfterObstacle = 150;
-        move == 2 ? (moveLeftAfterObstacle = 100) : moveLeftAfterObstacle;
-        move == 1 ? (moveLeftAfterObstacle = 50) : moveLeftAfterObstacle;
-        move == 0 ? (moveLeftAfterObstacle = 0) : moveLeftAfterObstacle;
-        celuleObstacle.forEach((obstacle) => {
-            if (
-                obstacle.width == player.width &&
-                obstacle.height + 50 == player.height
-            ) {
-                moveLeftAfterObstacle = 0;
-            } else if (
-                obstacle.width == player.width &&
-                obstacle.height + 50 == player.height - 50
-            ) {
-                moveLeftAfterObstacle = 50;
-            } else if (
-                obstacle.width == player.width &&
-                obstacle.height + 50 == player.height - 100
-            ) {
-                moveLeftAfterObstacle = 100;
+        let moveTopAfterObstacle = 150
+        move == 3 ? moveTopAfterObstacle = 150 : moveTopAfterObstacle
+        move == 2 ? moveTopAfterObstacle = 100 : moveTopAfterObstacle
+        move == 1 ? moveTopAfterObstacle = 50 : moveTopAfterObstacle
+        move == 0 ? moveTopAfterObstacle = 0 : moveTopAfterObstacle
+        console.log(moveTopAfterObstacle);
+
+        for (let index = 0; index < move; index++) {
+            moveLeft.push({
+                width,
+                height
+            })
+            height = height - 50
+            celuleObstacle.forEach(obstacle => {
+                if (obstacle.height == moveLeft[index].height && obstacle.width == moveLeft[index].width) {
+                    saveObstacle.push(obstacle.height)
+                }
+            });
+        }
+        console.log(moveLeft);
+
+        for (let y = 0; y < saveObstacle.length; y++) {
+            if (moveLeft[0].height == saveObstacle[0]) {
+                moveTopAfterObstacle = 0
+            } else if (moveLeft[0].height != saveObstacle[0]) {
+                if (moveLeft[1].height == saveObstacle[1] || moveLeft[1].height == saveObstacle[0]) {
+                    moveTopAfterObstacle = 50
+
+                } else if (moveLeft[1].height != saveObstacle[y]) {
+                    if (moveLeft[2].height == saveObstacle[y]) {
+                        moveTopAfterObstacle = 100
+
+
+                    } else if (
+                        moveLeft[2].height != saveObstacle[y] &&
+                        moveLeft[1].height != saveObstacle[y]
+                    ) {
+                        moveTopAfterObstacle = 150
+
+
+                    }
+                }
             }
-        });
-        player.move.top = moveLeftAfterObstacle;
-        /*
-    let moveLeft = [];
-    let width = player.width;
-    let height = player.height - 50;
-    let saveObstacle = [];
-    let moveTopAfterObstacle = 150;
-    move == 3 ? (moveTopAfterObstacle = 150) : moveTopAfterObstacle;
-    move == 2 ? (moveTopAfterObstacle = 100) : moveTopAfterObstacle;
-    move == 1 ? (moveTopAfterObstacle = 50) : moveTopAfterObstacle;
-    move == 0 ? (moveTopAfterObstacle = 0) : moveTopAfterObstacle;
-
-    for (let index = 0; index < move; index++) {
-      moveLeft.push({
-        width,
-        height,
-      });
-      height = height - 50;
-      celuleObstacle.forEach((obstacle) => {
-        if (
-          obstacle.height == moveLeft[index].height &&
-          obstacle.width == moveLeft[index].width
-        ) {
-          saveObstacle.push(obstacle.height);
         }
-      });
-    }
-
-    for (let y = 0; y < saveObstacle.length; y++) {
-      if (moveLeft[0].height == saveObstacle[0]) {
-        moveTopAfterObstacle = 0;
-      } else if (moveLeft[0].height != saveObstacle[0]) {
-        if (
-          moveLeft[1].height == saveObstacle[1] ||
-          moveLeft[1].height == saveObstacle[0]
-        ) {
-          moveTopAfterObstacle = 50;
-          move = 1;
-        } else if (moveLeft[1].height != saveObstacle[y]) {
-          if (moveLeft[2].height == saveObstacle[y]) {
-            moveTopAfterObstacle = 100;
-            move = 2;
-          } else if (
-            moveLeft[2].height != saveObstacle[y] &&
-            moveLeft[1].height != saveObstacle[y]
-          ) {
-            moveTopAfterObstacle = 150;
-            move = 3;
-          }
-        }
-      }
-    }
-    player.move.top = moveTopAfterObstacle;*/
-    }
+        player.move.top = moveTopAfterObstacle
+    };
     static makeDataMoveBottom(player, celuleObstacle, move) {
         let moveLeft = [];
         let width = player.width;
         let height = player.height + 50;
         let saveObstacle = [];
-        let moveBottomAfterObstacle = 150;
-        move == 3 ? (moveBottomAfterObstacle = 150) : moveBottomAfterObstacle;
-        move == 2 ? (moveBottomAfterObstacle = 100) : moveBottomAfterObstacle;
-        move == 1 ? (moveBottomAfterObstacle = 50) : moveBottomAfterObstacle;
-        move == 0 ? (moveBottomAfterObstacle = 0) : moveBottomAfterObstacle;
+        let moveBottomAfterObstacle = 150
+        move == 3 ? moveBottomAfterObstacle = 150 : moveBottomAfterObstacle
+        move == 2 ? moveBottomAfterObstacle = 100 : moveBottomAfterObstacle
+        move == 1 ? moveBottomAfterObstacle = 50 : moveBottomAfterObstacle
+        move == 0 ? moveBottomAfterObstacle = 0 : moveBottomAfterObstacle
         for (let index = 0; index < move; index++) {
             moveLeft.push({
                 width,
-                height,
-            });
+                height
+            })
+            console.log('MOVE LEFT just what i want ');
 
-            height = height + 50;
-            celuleObstacle.forEach((obstacle) => {
-                if (
-                    obstacle.height == moveLeft[index].height &&
-                    obstacle.width == moveLeft[index].width
-                ) {
-                    saveObstacle.push(obstacle.height);
+            console.log(moveLeft);
+            height = height + 50
+            celuleObstacle.forEach(obstacle => {
+                if (obstacle.height == moveLeft[index].height && obstacle.width == moveLeft[index].width) {
+                    saveObstacle.push(obstacle.height)
                 }
             });
+
         }
         for (let y = 0; y < saveObstacle.length; y++) {
             if (moveLeft[0].height == saveObstacle[0]) {
-                moveBottomAfterObstacle = 0;
+                moveBottomAfterObstacle = 0
             } else if (moveLeft[0].height != saveObstacle[0]) {
-                if (
-                    moveLeft[1].height == saveObstacle[1] ||
-                    moveLeft[1].height == saveObstacle[0]
-                ) {
-                    moveBottomAfterObstacle = 50;
-                    move = 1;
+                if (moveLeft[1].height == saveObstacle[1] || moveLeft[1].height == saveObstacle[0]) {
+                    moveBottomAfterObstacle = 50
+
+
                 } else if (moveLeft[1].height != saveObstacle[y]) {
                     if (moveLeft[2].height == saveObstacle[y]) {
-                        moveBottomAfterObstacle = 100;
-                        move = 2;
+                        moveBottomAfterObstacle = 100
+
                     } else if (
                         moveLeft[2].height != saveObstacle[y] &&
                         moveLeft[1].height != saveObstacle[y]
                     ) {
-                        moveBottomAfterObstacle = 150;
-                        move = 3;
+                        moveBottomAfterObstacle = 150
+
+
                     }
                 }
             }
         }
-        player.move.down = moveBottomAfterObstacle;
-    }
+        player.move.down = moveBottomAfterObstacle
+    };
     static makeDataMoveRight(player, celuleObstacle, move) {
         let moveLeft = [];
         let width = player.width + 50;
         let height = player.height;
         let saveObstacle = [];
-        let moveRightAfterObstacle = 150;
+        let moveRightAfterObstacle = 150
 
-        move == 2 ? (moveRightAfterObstacle = 100) : moveRightAfterObstacle;
-        move == 1 ? (moveRightAfterObstacle = 50) : moveRightAfterObstacle;
-        move == 0 ? (moveRightAfterObstacle = 0) : moveRightAfterObstacle;
+        move == 2 ? moveRightAfterObstacle = 100 : moveRightAfterObstacle
+        move == 1 ? moveRightAfterObstacle = 50 : moveRightAfterObstacle
+        move == 0 ? moveRightAfterObstacle = 0 : moveRightAfterObstacle
+        console.log(moveRightAfterObstacle);
         for (let index = 0; index < move; index++) {
             moveLeft.push({
                 width,
-                height,
-            });
-            width = width + 50;
-            celuleObstacle.forEach((obstacle) => {
-                if (
-                    obstacle.width == moveLeft[index].width &&
-                    obstacle.height == moveLeft[index].height
-                ) {
-                    saveObstacle.push(obstacle.width);
+                height
+            })
+            width = width + 50
+            celuleObstacle.forEach(obstacle => {
+                if (obstacle.width == moveLeft[index].width && obstacle.height == moveLeft[index].height) {
+                    saveObstacle.push(obstacle.width)
                 }
             });
+
         }
         for (let y = 0; y < saveObstacle.length; y++) {
             if (moveLeft[0].width == saveObstacle[0]) {
-                moveRightAfterObstacle = 0;
+                moveRightAfterObstacle = 0
             } else if (moveLeft[0].width != saveObstacle[0]) {
-                if (
-                    moveLeft[1].width == saveObstacle[1] ||
-                    moveLeft[1].width == saveObstacle[0]
-                ) {
-                    moveRightAfterObstacle = 50;
+                if (moveLeft[1].width == saveObstacle[1] || moveLeft[1].width == saveObstacle[0]) {
+                    moveRightAfterObstacle = 50
                 } else if (moveLeft[1].width != saveObstacle[y]) {
                     if (moveLeft[2].width == saveObstacle[y]) {
-                        moveRightAfterObstacle = 100;
+                        moveRightAfterObstacle = 100
                     } else if (
                         moveLeft[2].width != saveObstacle[y] &&
                         moveLeft[1].width != saveObstacle[y]
                     ) {
-                        moveRightAfterObstacle = 150;
+                        moveRightAfterObstacle = 150
                     }
                 }
             }
         }
-        player.move.right = moveRightAfterObstacle;
-    }
+        player.move.right = moveRightAfterObstacle
+    };
     static makeDataMovePlayer(player, celuleObstacle, move) {
-        this.makeDataMoveleft(player, celuleObstacle, move);
-        this.makeDataMoveTop(player, celuleObstacle, move);
-        this.makeDataMoveBottom(player, celuleObstacle, move);
-        this.makeDataMoveRight(player, celuleObstacle, move);
+        this.makeDataMoveleft(player, celuleObstacle, move)
+        this.makeDataMoveTop(player, celuleObstacle, move)
+        this.makeDataMoveBottom(player, celuleObstacle, move)
+        this.makeDataMoveRight(player, celuleObstacle, move)
     }
+
 }
 
-export default Move;
+export default Move
